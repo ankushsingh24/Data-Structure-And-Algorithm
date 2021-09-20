2e1d6df4ece6cdc210bf028309da01a51bc4c06e
Ankush Singh <ankushsahil45@gmail.com>
2021-09-21 00:23:10 +0530

    added sum using recursion

---
+++ Recursion/sum_of_n_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n)
+{
+    if (n > 0)
+    {
+        return n + sum(n - 1);
+    }
+    return 0;
+}
+
+int main()
+{
+    cout << sum(10);
+    return 0;
+}
