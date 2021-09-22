7bf6dfca26e128768cb6405452744ee38057ee73
Ankush Singh <ankushsahil45@gmail.com>
2021-09-22 23:33:37 +0530

    taylor series using recursion

--- Recursion/taylor_series.cpp
+++ Recursion/taylor_series.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 using namespace std;
 
+// Taylor Series
+// e^x = 1 + x/1 + x^2/2! + x^3/3! + ...... + x^n/n!
+
+double taylorSeries(int x, int n)
+{
+    static double p = 1, f = 1;
+    double r;
+
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        r = taylorSeries(x, n - 1);
+        p = p * x;
+        f = f * n;
+
+        return (r + p / f);
+    }
+}
+
 int main()
 {
-
+    double a = taylorSeries(4, 20);
+    cout << a;
     return 0;
 }
