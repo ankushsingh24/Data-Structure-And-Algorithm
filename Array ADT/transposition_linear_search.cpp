fd959f5ade021b6d566fe9425382bd3e6926783a
Ankush Singh <ankushsahil45@gmail.com>
2021-10-03 01:03:46 +0530

    added other ways of linear search

---
+++ Array ADT/transposition_linear_search.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int element)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == element)
+        {
+            swap(arr[i], arr[i - 1]);
+            return i;
+        }
+    }
+    return -1;
+}
+
+void display(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int n, element;
+
+    cout << "Enter the number of elements : ";
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int size = sizeof(arr) / sizeof(int);
+
+    cout << "Enter the number to be searched : ";
+    cin >> element;
+
+    int searchIndex = linearSearch(arr, size, element);
+
+    cout << "The element " << element << " is found at index : " << searchIndex << endl;
+
+    cout << "Displaying Elements : ";
+    display(arr, size);
+
+    return 0;
+}
