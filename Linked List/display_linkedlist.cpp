84d11899ecc0f495d0e55984bac7fee5488399b3
Ankush Singh <ankushsahil45@gmail.com>
2021-11-13 21:39:53 +0530

    add LinkedList Traversing

---
+++ Linked List/display_linkedlist.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << "\t";
+        // cout << p->next << "\t" << endl;
+        p = p->next;
+    }
+}
+
+int main()
+{
+    int A[] = {3, 5, 7, 10, 15};
+    create(A, 5);
+    display(first);
+    return 0;
+}
