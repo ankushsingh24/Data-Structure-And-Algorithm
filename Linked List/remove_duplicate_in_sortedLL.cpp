6a225a2092276c3bca3fe569f54ab1c274f2ff2e
Ankush Singh <ankushsahil45@gmail.com>
2021-12-16 21:43:25 +0530

    added remove duplicate in LL

---
+++ Linked List/remove_duplicate_in_sortedLL.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        cout << p->data << "\t";
+        p = p->next;
+    }
+}
+
+void removeDuplicate(struct Node *p)
+{
+    struct Node *q = p->next;
+
+    while (q != NULL)
+    {
+        if (p->data != q->data)
+        {
+            p = q;
+            q = q->next;
+        }
+        else
+        {
+            p->next = q->next;
+            free(q);
+            q = p->next;
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {10, 3, 3, 40, 50};
+    create(A, 5);
+    display(first);
+
+    cout << endl;
+
+    removeDuplicate(first);
+    display(first);
+
+    return 0;
+}
