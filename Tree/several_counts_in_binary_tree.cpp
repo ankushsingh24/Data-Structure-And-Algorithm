164330b7291130cea2e3752de47a1c81deeb87d7
Ankush Singh <ankushsahil45@gmail.com>
2022-03-01 22:39:44 +0530

    added several counts in BT

--- Tree/several_counts_in_binary_tree.cpp
+++ Tree/several_counts_in_binary_tree.cpp
@@ -109,11 +109,17 @@ int count_nodes(Node *root)
     x = count_nodes(root->lchild);
     y = count_nodes(root->rchild);
     return x + y + 1;
+
+    /*
+        if (root == NULL)
+            return 0;
+        return count_nodes(root->lchild) + count_nodes(root->rchild) + 1;
+    */
 }
 
 int height(Node *root)
 {
-    int x, y;
+    int x = 0, y = 0;
     if (root == NULL)
         return 0;
 
@@ -126,12 +132,61 @@ int height(Node *root)
         return y + 1;
 }
 
+int nodesWithDegree0(Node *root)
+{
+    int x, y;
+    if (root == NULL)
+        return 0;
+
+    x = nodesWithDegree0(root->lchild);
+    y = nodesWithDegree0(root->rchild);
+
+    if (root->lchild == NULL && root->rchild == NULL)
+        return x + y + 1;
+    else
+        return x + y;
+}
+
+int nodesWithDegree1(Node *root)
+{
+    int x, y;
+    if (root == NULL)
+        return 0;
+
+    x = nodesWithDegree1(root->lchild);
+    y = nodesWithDegree1(root->rchild);
+
+    // if (root->lchild == NULL && root->rchild || root->lchild && root->rchild == NULL)
+    if ((root->lchild != NULL) ^ (root->rchild != NULL))
+        return x + y + 1;
+    else
+        return x + y;
+}
+
+int nodesWithDegree2(Node *root)
+{
+    int x, y;
+    if (root == NULL)
+        return 0;
+
+    x = nodesWithDegree2(root->lchild);
+    y = nodesWithDegree2(root->rchild);
+
+    if (root->lchild && root->rchild)
+        return x + y + 1;
+    else
+        return x + y;
+}
+
 int main()
 {
     Treecreate();
 
     cout << "\nNo of nodes : " << count_nodes(root);
     cout << "\nHeight : " << height(root);
+    cout << "\nNodes with Degree 0 : " << nodesWithDegree0(root);
+    cout << "\nNodes with Degree 1 : " << nodesWithDegree1(root);
+    cout << "\nNodes with Degree 2 : " << nodesWithDegree2(root);
 
     return 0;
 }
