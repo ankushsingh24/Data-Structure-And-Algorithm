acdb8b70920da0d36a759b7dad955e7556ac06b3
Ankush Singh <ankushsahil45@gmail.com>
2022-07-26 22:51:36 +0530

    updated Insertion Sort

--- Sorting/Insertion-Sort.cpp
+++ Sorting/Insertion-Sort.cpp
@@ -23,6 +23,15 @@ void InsertionSort(int A[], int n)
 
 int main()
 {
+    int A[] = {3, 7, 9, 10, 6, 5, 12, 4, 11, 2};
+    int n = 10;
+
+    InsertionSort(A, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
 
     return 0;
 }
